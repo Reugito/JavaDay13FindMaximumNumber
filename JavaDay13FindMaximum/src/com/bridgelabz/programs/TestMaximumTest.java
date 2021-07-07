28cd06ec07e6586ebca4e8df0142048c346fce06
Reugito <raosahebdhotreedu@gmail.com>
2021-07-07 07:57:47 +0530

    Taking morthan three generic parameters and checking the max number or string

--- JavaDay13FindMaximum/src/com/bridgelabz/programs/TestMaximumTest.java
+++ JavaDay13FindMaximum/src/com/bridgelabz/programs/TestMaximumTest.java
@@ -4,24 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestMaximumTest {
-	TestMaximum testMax = new TestMaximum();
+class TestMaximumTest <t> {
+	TestMaximum testInt = new TestMaximum();
 	
-	@Test
-	void testGetMaxNumberFirstPosition() {
-		String maxStr = testMax.getMaxStr("hello", "hi", "h");
-		assertEquals("hello", maxStr);
+	@Test      
+	void testGetMaximumNumber() {
+		t maxInt = (t) testInt.getMaximumNumber(1, 2, 3, 4, 5);
+		assertEquals(5, maxInt);
 	}
 	
-	@Test
-	void testGetMaxNumberSecondPosition() {
-		String maxStr = testMax.getMaxStr("hi", "hello", "h");
-		assertEquals("hello", maxStr);
+	@Test      
+	void testGetMaximumStr() {
+		t maxStr = (t) testInt.getMaximumNumber("hi", "hello", "welcome", "me");
+		assertEquals("welcome", maxStr);
 	}
 	
-	@Test
-	void testGetMaxNumberThirdPosition() {
-		String maxStr = testMax.getMaxStr("hi", "h", "hello");
+	@Test      
+	void testGetMaximumDouble() {
+		t maxDouble = (t) testInt.getMaximumNumber(1.2, 45.9, 112.7, 12123.5);
+		assertEquals( 12123.5, maxDouble);
 	}
-
 }
